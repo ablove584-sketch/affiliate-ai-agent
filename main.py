3c52584912ef801572d104240ad936e8c91a3656
ablove584-sketch <ablove584@gmail.com>
2026-08-26 23:36:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # تهيئة الذكاء الاصطناعي (Gemini)
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash-latest')
+model = genai.GenerativeModel('gemini-pro')
 
 async def generate_ad():
     """يطلب من الذكاء الاصطناعي كتابة إعلان لمنتج عشوائي"""
